bb8b488fbb3159c12ed8fc904741efee6fc8bd57
Daniel Fliguel Guz <daniel.fliguel@tbfconsultoria.com.br>
2019-10-19 21:57:59 -0300

    Criação da página 'Especiaidades, inclusão do jquery e alterações no css'

---
+++ js/script.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+	$('#button-1').on('click',function(){
+		 
+		$('#span-1').toggleClass('none');
+		$('#card-1').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-2').on('click',function(){
+		 
+		$('#span-2').toggleClass('none');
+		$('#card-2').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-3').on('click',function(){
+		 
+		$('#span-3').toggleClass('none');
+		$('#card-3').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-4').on('click',function(){
+		 
+		$('#span-4').toggleClass('none');
+		$('#card-4').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-5').on('click',function(){
+		 
+		$('#span-5').toggleClass('none');
+		$('#card-5').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-6').on('click',function(){
+		 
+		$('#span-6').toggleClass('none');
+		$('#card-6').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-7').on('click',function(){
+		 
+		$('#span-7').toggleClass('none');
+		$('#card-7').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+	$('#button-8').on('click',function(){
+		 
+		$('#span-8').toggleClass('none');
+		$('#card-8').toggleClass('max-height');
+		if ($(this).html()=='SAIBA MAIS'){
+			$(this).html('LER MENOS');	
+		}
+		else{
+			$(this).html('SAIBA MAIS');				
+		}		
+	});
+
+});
+
+
